e4ab8137d86cc5fce60d3f61f96013a560706d39
reidMHAC <reidelkins@outlook.com>
2022-09-21 23:39:52 -0500

    fixed error

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -16,7 +16,7 @@ const Product = () => {
           {product.title.split(' ').map((word, index) => (
             <span
               key={index}
-              className={index % 2 ? 'text-primary' : 'text-border'}
+              className={index % 2 ? 'text-border' : 'text-primary'}
             >
               {word}{' '}
             </span>
