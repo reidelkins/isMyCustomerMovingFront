1f36e7adc5139b4f8011da49e4fbbc308a32e09e
reidMHAC <reidelkins@outlook.com>
2022-12-01 21:36:05 -0600

    combine checkout pages

--- src/pages/checkout/[time]/[tier].tsx
+++ src/pages/checkout/[time]/[tier].tsx
@@ -1,8 +1,27 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js/pure";
 import { useEffect, useState } from "react";
-import Checkout from "../../../components/Checkout";
+import { Container, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+import CheckoutForm from "../../../components/CheckoutForm";
 import ApiService from "../../api/checkout";
+
+const RootStyle = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+  },
+}));
+
+const ContentStyle = styled("div")(({ theme }) => ({
+  maxWidth: 480,
+  margin: "auto",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  padding: theme.spacing(12, 0),
+}));
 
 export default function CheckoutPage() {
   const [stripePromise, setStripePromise] = useState<any>(null);
@@ -20,12 +39,24 @@ export default function CheckoutPage() {
   }, []);
 
   return (
-    <div>
-      {stripePromise && clientSecret && (
-        <Elements stripe={stripePromise} options={{ clientSecret }}>
-          <Checkout />
-        </Elements>
-      )}
-    </div>
+    <section title="Checkout">
+      <RootStyle>
+        <div className="lg:w-1/2 flex items-center justify-center bg-primary border">
+          <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
+            Welcome Back
+          </Typography>
+        </div>
+
+        <Container maxWidth="sm">
+          <ContentStyle>
+            {stripePromise && clientSecret && (
+              <Elements stripe={stripePromise} options={{ clientSecret }}>
+                <CheckoutForm />
+              </Elements>
+            )}
+          </ContentStyle>
+        </Container>
+      </RootStyle>
+    </section>
   );
 }
